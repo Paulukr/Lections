ab1044181a1f2d2202a3e4b5d716e9ebae9f4103
Ivan <Ivan@Ivan-PC>
2016-10-31 20:17:06 +0200

    Initial commit

---
+++ Lections/src/lection1/Sizeof.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lection1;
+
+/**
+ * @author Ivan
+ *
+ */
+final class Sizeof {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
